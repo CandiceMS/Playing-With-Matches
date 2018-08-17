824a700504ac42c94b6c4eb8892291b3c3893498
Candice <candice.m.sprout@gmail.com>
2018-08-17 15:58:56 +0200

    Fourth commit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 // console.log(number);
 // console.log(number[0]);
 // console.log(number[1]);
+let display = document.querySelector('.displayAlert');
+let reloadBtn = document.querySelector('button[name="reload"]');
+console.log(reloadBtn);
 
   function randomNumber(max) {
     return Math.ceil(Math.random() * Math.floor(9));
@@ -16,10 +19,33 @@
     // Alternative: create new variables/instances.
     // let indexList = number[i];
     // let random = randomNumber();
-
+    matchNumbers();
+}
     function matchNumbers() {
-      
-    }
-  }
+      let sameNumber1 = number[0];
+      let sameNumber2 = number[1];
+      let sameNumber3 = number[2];
 
-  loopNumbers(randomNumber());
+      if (sameNumber2.innerHTML === sameNumber1.innerHTML) {
+         sameNumber1.classList.add("highlight");
+        sameNumber2.classList.add("highlight");
+        display.innerHTML = "You have a match!";
+      }
+      if (sameNumber2.innerHTML === sameNumber3.innerHTML) {
+        sameNumber2.classList.add("highlight");
+      sameNumber3.classList.add("highlight");
+      display.innerHTML = "You have a match!";
+      }
+      else if(sameNumber2.innerHTML === sameNumber1.innerHTML && sameNumber2.innerHTML === sameNumber3.innerHTML ){
+        sameNumber1.classList.add("highlight");
+        sameNumber2.classList.add("highlight");
+        sameNumber3.classList.add("highlight");
+        display.innerHTML = "You have a triple match!";
+      }
+    }
+
+   loopNumbers(randomNumber());
+
+  reloadBtn.addEventListener('click', function() {
+      loopNumbers(randomNumber());
+  });
